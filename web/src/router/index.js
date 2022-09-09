c7d8867afe50d83a5704ca1261ddf7c8b81e2b8d
RainSure <1114560341@qq.com>
2022-09-09 12:28:15 +0800

    添加注册功能以及身份可持久化

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -6,41 +6,63 @@ import UserBotIndexView from '@/views/user/bots/UserBotIndexView.vue'
 import NotFound from '@/views/error/NotFound.vue'
 import UserAccountLoginView from '@/views/user/account/UserAccountLoginView.vue'
 import UserAccountRegisterView from '@/views/user/account/UserAccountRegisterView.vue'
+import store from '@/store'
 const routes = [
   {
     path: "/",
     name: "home",
     redirect: "/pk/",
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/pk/",
     name: "pk_index",
     component: PkIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/record/",
     name: "record_index",
     component: RecordIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/ranklist/",
     name: "ranklist_index",
-    component: RanklistIndexView
+    component: RanklistIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/user/bot/",
     name: "user_bot_index",
     component: UserBotIndexView,
+    meta: {
+      requestAuth: true,
+    }
   },
   {
     path: "/user/account/login/",
     name: "user_account_login",
     component: UserAccountLoginView,
+    meta: {
+      requestAuth: false,
+    }
   },
   {
     path: "/user/account/register/",
     name: "user_account_register",
     component: UserAccountRegisterView,
+    meta: {
+      requestAuth: false,
+    }
   },
   {
     path: "/404/",
@@ -57,6 +79,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if(to.meta.requestAuth && !store.state.user.is_login) {
+    next({name: "user_account_login"});
+  }else {
+    next();
+  }
 })
 
 export default router
